151d14fc61a08e78933fd0bb3c5e8257a62b8256
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-08-09 16:20:25 +0530

    [mohitkumar9884] adding/creating branch SimpleLinkedListUC1

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Linked List Program");
+            Console.WriteLine("Welcome To Linked List Program.");
+            Console.WriteLine("Please choose any one program from below options");
+            Console.WriteLine("1.Builtin Linked list\n2.Custom Linked List");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    LinkedList<int> linkedList = new LinkedList<int>();
+                    linkedList.AddLast(10);
+                    linkedList.AddLast(20);
+
+                    Console.WriteLine("\nDisplaying nodes");
+                    foreach (int element in linkedList)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    linkedList.AddFirst(30);
+
+                    Console.WriteLine("\nDisplaying nodes");
+                    foreach (int element in linkedList)
+                    {
+                        Console.WriteLine(element);
+                    }
+                    linkedList.RemoveLast();
+
+                    Console.WriteLine("\nDisplaying nodes");
+                    foreach (int element in linkedList)
+                    {
+                        Console.WriteLine(element);
+                    }
+                break;
+                case 2:
+
+
+            }
+            
         }
     }
 }
